d1ba3eb09d13e7cd777f0d2c0582570253bad5cf
Amin <amin.bw95@gmail.com>
2020-05-10 11:58:54 +0430

    adding blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    pub_date=models.DateField()
+    body=models.TextField()
+    image=models.ImageField()
+#title
+#pub_date
+#body
+#image
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amin/source codes/python/Django/portfolio-project/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
