473c2ed50fc9c1c3e9059435c62716971fda5bf5
Amin <amin.bw95@gmail.com>
2020-05-10 19:47:45 +0430

    the first complete website

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,9 @@ class Blog(models.Model):
     pub_date=models.DateTimeField()
     body=models.TextField()
     image=models.ImageField(upload_to='images/')
-#title
-#pub_date
-#body
-#image
+    def summary(self):
+        return self.body[:100]
+    def pub_date_short(self):
+        return self.pub_date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
