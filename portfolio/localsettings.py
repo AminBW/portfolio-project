f9b3e1d6f54c33534161b623336998ecaea48add
Amin <amin.bw95@gmail.com>
2020-05-12 13:33:13 +0430

    requirments and localsetting

---
+++ portfolio/localsettings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = '#q!hi3#nfpmko-1mihrxv7osc!a&p8gzwlb)f*ugy3&cbeamx*3rd*5c'
+ALLOWED_HOSTS = ['aminbw.pythonanywhere.com']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfoliodb',
+        'USER': 'aminbw',
+        'PASSWORD': 'Amin@113733',
+        'HOST': '',
+        'PORT': ''
+    }
+}
